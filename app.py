9ef4a1c505b4fe89c63b5a32d2a26cd31238475b
Kripa Sharma <2101030400376@silveroakuni.ac.in>
2025-04-29 12:13:19 +0530

    predication

--- app.py
+++ app.py
@@ -28,8 +28,8 @@ def predication_datapoint():
             lunch=request.form.get('lunch'),
             parental_level_of_education=request.form.get('parental_level_of_education'),
             test_preparation_course=request.form.get('test_preparation_course'),
-            reading_score=float(request.form.get('reading_score')),
-            writing_score=float(request.form.get('writing_score'))       
+            reading_score=request.form.get('reading_score'),
+            writing_score=request.form.get('writing_score')      
             )
         
         pred_df=data.get_data_as_data_frame()
@@ -37,12 +37,14 @@ def predication_datapoint():
         print("Before Predications")
 
         predict_pipeline=PredictPipeline()
+        print(predict_pipeline)
         print("mid predicayions")
 
         final=predict_pipeline.predict(pred_df)
+        print(final)
         print("after predications")
         
-        return render_template("home.html",final=final[0])
+        return render_template("home.html",final=int(final[0]))
     
 
 if __name__=='__main__':
